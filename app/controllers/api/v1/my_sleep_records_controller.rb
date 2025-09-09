3d4dd73431cf6a4fbea588f6ae6b48a73ac1c4bc
Ainul Fitriyah <ainul.fitriyah@mekari.com>
2025-09-10 06:47:20 +0700

    adjust the create sleep record process also update duration_second

--- app/controllers/api/v1/my_sleep_records_controller.rb
+++ app/controllers/api/v1/my_sleep_records_controller.rb
@@ -7,7 +7,11 @@ class Api::V1::MySleepRecordsController < ApplicationController
   def create
     sleep_record_active = @current_user.sleep_records.active.first
     if sleep_record_active
-      sleep_record_active.update(woke_at: DateTime.now)
+      date_now = Time.now
+      duration_seconds = (date_now - sleep_record_active.slept_at).to_i
+      sleep_record_active.update(
+        woke_at: date_now,
+        duration_seconds: duration_seconds)
       render json: { message: "Sleep record ended", sleep_record: sleep_record_active }, status: :ok
     else
       sleep_record = @current_user.sleep_records.create(slept_at: DateTime.now)
