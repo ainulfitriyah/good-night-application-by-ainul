8e429c586426f670061d7d1bb61c20e003136307
Ainul Fitriyah <ainul.fitriyah@mekari.com>
2025-09-12 08:24:09 +0700

    change unit test get list my_sleep_records to type request

--- spec/request/api/v1/user_get_list_my_sleep_record_request_spec.rb
+++ spec/request/api/v1/user_get_list_my_sleep_record_request_spec.rb
@@ -1,13 +1,13 @@
 require 'rails_helper'
 
-RSpec.describe Api::V1::MySleepRecordsController, type: :controller do
+RSpec.describe "Api::V1::MySleepRecords", type: :request do
   let(:user) { create(:user) }
   let!(:sleep_record1) { create(:sleep_record, user: user, slept_at: 2.days.ago) }
   let!(:sleep_record2) { create(:sleep_record, user: user, slept_at: 1.day.ago) }
 
   describe "GET /api/v1/users/:user_id/my_sleep_records" do
     it "returns paginated sleep records" do
-      get :index, params: { user_id: user.id, page: 1, per_page: 1 }
+      get "/api/v1/users/#{user.id}/my_sleep_records", params: { page: 1, per_page: 1 }
       expect(response).to have_http_status(:ok)
       json = JSON.parse(response.body)
       expect(json['sleep_records'].length).to eq(1)
@@ -15,7 +15,7 @@ RSpec.describe Api::V1::MySleepRecordsController, type: :controller do
     end
 
     it "filters by date range" do
-      get :index, params: {
+      get "/api/v1/users/#{user.id}/my_sleep_records", params: {
         user_id: user.id,
         start_date: 3.days.ago.to_date.to_s,
         end_date: 1.day.ago.to_date.to_s
